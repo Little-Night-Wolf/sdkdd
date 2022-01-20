2a376349025a24f31382f544260c4cb1d4a4c4da
Anonymous <>
2022-01-19 19:15:25 -0500

    fix: ezfix2

--- ezfix2.py
+++ ezfix2.py
@@ -9,7 +9,7 @@ import requests
 from psycopg2.extras import RealDictCursor
 from bs4 import BeautifulSoup
 
-sqlite_conn = sqlite3.connect('`/root/migration_prep/baseline/processing.db`')
+sqlite_conn = sqlite3.connect('/root/migration_prep/baseline/processing.db')
 
 posts_to_fix = sqlite_conn.execute('''
     SELECT
@@ -35,7 +35,7 @@ for (post_service, post_user_id, post_id, old_file_location, new_file_location) 
     new_file_hash = os.path.splitext(os.path.basename(new_file_location))
     old_filename = os.path.basename(old_file_location)
 
-    cursor = conn.cursor()
+    cursor = psql_conn.cursor()
     cursor.execute("""
         INSERT INTO file_post_relationships (file_id, filename, service, \"user\", post, inline)
         VALUES ((SELECT id FROM files WHERE hash = %(hash)s), %(filename)s, %(service)s, %(user)s, %(post)s, %(inline)s) ON CONFLICT DO NOTHING
