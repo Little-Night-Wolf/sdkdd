098b1fc2a480f42eb4736d90a5e29ef8d4c89b60
Anonymous <>
2022-01-29 20:01:04 -0500

    fix(hashfixer): sql

--- hashfixer.py
+++ hashfixer.py
@@ -51,88 +51,96 @@ with open('./shinofix.txt', 'r') as f:
                 print(f"File entry fixed ({old_path} > {correct_path})")
 
                 # Find posts that contain this file and replace in its data, just to be sure
-                cursor.execute('''
-                    WITH rels as (SELECT * FROM file_post_relationships WHERE file_id = (SELECT id FROM files WHERE hash = %s))
-                    SELECT *
-                    FROM posts, rels
-                    WHERE
-                        posts.service = rels.service
-                        AND posts."user" = rels."user"
-                        AND posts.id = rels.post
-                ''', (correct_hash,))
-                posts_to_scrub = cursor.fetchall()
+                with conn.cursor() as cursor:
+                    cursor.execute('SELECT * FROM file_post_relationships WHERE file_id = (SELECT id FROM files WHERE hash = %s)', (correct_hash,))
+                    relationships_to_scrub = cursor.fetchall()
+                    for relationship in relationships_to_scrub:
+                        with conn.cursor() as cursor:
+                            cursor.execute('''
+                                SELECT *
+                                FROM posts
+                                WHERE
+                                    posts.service = %s
+                                    AND posts."user" = %s
+                                    AND posts.id = %s
+                            ''', (relationship['service'], relationship['user'], relationship['id']))
+                            posts_to_scrub = cursor.fetchall()
 
-                for post in posts_to_scrub:
-                    post['content'] = post['content'].replace('https://kemono.party' + old_path, correct_path)
-                    post['content'] = post['content'].replace(old_path, correct_path)
-                    if post['file'].get('path'):
-                        post['file']['path'] = post['file']['path'].replace('https://kemono.party' + old_path, correct_path)
-                        post['file']['path'] = post['file']['path'].replace(old_path, correct_path)
-                    for (i, _) in enumerate(post['attachments']):
-                        if post['attachments'][i].get('path'): # not truely needed, but...
-                            post['attachments'][i]['path'] = post['attachments'][i]['path'].replace('https://kemono.party' + old_path, correct_path)
-                            post['attachments'][i]['path'] = post['attachments'][i]['path'].replace(old_path, correct_path)
+                            for post in posts_to_scrub:
+                                post['content'] = post['content'].replace('https://kemono.party' + old_path, correct_path)
+                                post['content'] = post['content'].replace(old_path, correct_path)
+                                if post['file'].get('path'):
+                                    post['file']['path'] = post['file']['path'].replace('https://kemono.party' + old_path, correct_path)
+                                    post['file']['path'] = post['file']['path'].replace(old_path, correct_path)
+                                for (i, _) in enumerate(post['attachments']):
+                                    if post['attachments'][i].get('path'): # not truely needed, but...
+                                        post['attachments'][i]['path'] = post['attachments'][i]['path'].replace('https://kemono.party' + old_path, correct_path)
+                                        post['attachments'][i]['path'] = post['attachments'][i]['path'].replace(old_path, correct_path)
 
-                    # format
-                    post['embed'] = json.dumps(post['embed'])
-                    post['file'] = json.dumps(post['file'])
-                    for i in range(len(post['attachments'])):
-                        post['attachments'][i] = json.dumps(post['attachments'][i])
+                                # format
+                                post['embed'] = json.dumps(post['embed'])
+                                post['file'] = json.dumps(post['file'])
+                                for i in range(len(post['attachments'])):
+                                    post['attachments'][i] = json.dumps(post['attachments'][i])
 
-                    # update
-                    columns = post.keys()
-                    data = ['%s'] * len(post.values())
-                    data[list(columns).index('attachments')] = '%s::jsonb[]'  # attachments
-                    query = 'UPDATE posts SET {updates} WHERE {conditions}'.format(
-                        updates=','.join([f'"{column}" = {data[i]}' for (i, column) in enumerate(columns)]),
-                        conditions='service = %s AND "user" = %s AND id = %s'
-                    )
-                    cursor.execute(query, list(post.values()) + list((post['service'], post['user'], post['id'],)))
+                                # update
+                                columns = post.keys()
+                                data = ['%s'] * len(post.values())
+                                data[list(columns).index('attachments')] = '%s::jsonb[]'  # attachments
+                                query = 'UPDATE posts SET {updates} WHERE {conditions}'.format(
+                                    updates=','.join([f'"{column}" = {data[i]}' for (i, column) in enumerate(columns)]),
+                                    conditions='service = %s AND "user" = %s AND id = %s'
+                                )
+                                cursor.execute(query, list(post.values()) + list((post['service'], post['user'], post['id'],)))
 
-                    print(f"{post['service']}/{post['user']}/{post['id']} fixed ({old_path} -> {correct_path})")
-                    if (not config.dry_run):
-                        requests.request('BAN', f"{config.ban_url}/{post['service']}/user/{post['user']}")
+                                print(f"{post['service']}/{post['user']}/{post['id']} fixed ({old_path} -> {correct_path})")
+                                if (not config.dry_run):
+                                    requests.request('BAN', f"{config.ban_url}/{post['service']}/user/{post['user']}")
 
                 # DICKSWORD
-                cursor.execute('''
-                    WITH rels as (SELECT * FROM file_discord_message_relationships WHERE file_id = (SELECT id FROM files WHERE hash = %s))
-                    SELECT *
-                    FROM discord_posts, rels
-                    WHERE
-                        discord_posts.server = rels.server
-                        AND discord_posts.channel = rels.channel
-                        AND discord_posts.id = rels.id
-                ''', (correct_hash,))
-                messages_to_scrub = cursor.fetchall()
+                with conn.cursor() as cursor:
+                    cursor.execute('SELECT * FROM file_discord_message_relationships WHERE file_id = (SELECT id FROM files WHERE hash = %s)', (correct_hash,))
+                    relationships_to_scrub = cursor.fetchall()
+                    for relationship in relationships_to_scrub:
+                        with conn.cursor() as cursor:
+                            cursor.execute('''
+                                SELECT *
+                                FROM discord_posts
+                                WHERE
+                                    discord_posts.server = rels.server
+                                    AND discord_posts.channel = rels.channel
+                                    AND discord_posts.id = rels.id
+                            ''', (relationship['server'], relationship['channel'], relationship['id']))
+                            messages_to_scrub = cursor.fetchall()
 
-                for message in messages_to_scrub:
-                    for (i, _) in enumerate(message['attachments']):
-                        if message['attachments'][i].get('path'): # not truely needed, but...
-                            message['attachments'][i]['path'] = message['attachments'][i]['path'].replace('https://kemono.party' + old_path, correct_path)
-                            message['attachments'][i]['path'] = message['attachments'][i]['path'].replace(old_path, correct_path)
+                            for message in messages_to_scrub:
+                                for (i, _) in enumerate(message['attachments']):
+                                    if message['attachments'][i].get('path'): # not truely needed, but...
+                                        message['attachments'][i]['path'] = message['attachments'][i]['path'].replace('https://kemono.party' + old_path, correct_path)
+                                        message['attachments'][i]['path'] = message['attachments'][i]['path'].replace(old_path, correct_path)
 
-                    # format
-                    message['author'] = json.dumps(message['author'])
-                    for i in range(len(message['mentions'])):
-                        message['mentions'][i] = json.dumps(message['mentions'][i])
-                    for i in range(len(message['attachments'])):
-                        message['attachments'][i] = json.dumps(message['attachments'][i])
-                    for i in range(len(message['embeds'])):
-                        message['embeds'][i] = json.dumps(message['embeds'][i])
+                                    # format
+                                    message['author'] = json.dumps(message['author'])
+                                    for i in range(len(message['mentions'])):
+                                        message['mentions'][i] = json.dumps(message['mentions'][i])
+                                    for i in range(len(message['attachments'])):
+                                        message['attachments'][i] = json.dumps(message['attachments'][i])
+                                    for i in range(len(message['embeds'])):
+                                        message['embeds'][i] = json.dumps(message['embeds'][i])
 
-                    # update
-                    columns = message.keys()
-                    data = ['%s'] * len(message.values())
-                    data[list(columns).index('mentions')] = '%s::jsonb[]'  # mentions
-                    data[list(columns).index('attachments')] = '%s::jsonb[]'  # attachments
-                    data[list(columns).index('embeds')] = '%s::jsonb[]'  # embeds
-                    query = 'UPDATE discord_posts SET {updates} WHERE {conditions}'.format(
-                        updates=','.join([f'"{column}" = {data[i]}' for (i, column) in enumerate(columns)]),
-                        conditions='server = %s AND channel = %s AND id = %s'
-                    )
-                    cursor.execute(query, list(message.values()) + list((message['server'], message['channel'], message['id'],)))
+                                    # update
+                                    columns = message.keys()
+                                    data = ['%s'] * len(message.values())
+                                    data[list(columns).index('mentions')] = '%s::jsonb[]'  # mentions
+                                    data[list(columns).index('attachments')] = '%s::jsonb[]'  # attachments
+                                    data[list(columns).index('embeds')] = '%s::jsonb[]'  # embeds
+                                    query = 'UPDATE discord_posts SET {updates} WHERE {conditions}'.format(
+                                        updates=','.join([f'"{column}" = {data[i]}' for (i, column) in enumerate(columns)]),
+                                        conditions='server = %s AND channel = %s AND id = %s'
+                                    )
+                                    cursor.execute(query, list(message.values()) + list((message['server'], message['channel'], message['id'],)))
 
-                    print(f"discord: {message['server']}/{message['channel']}/{message['id']} fixed ({old_path} -> {correct_path})")
+                                    print(f"discord: {message['server']}/{message['channel']}/{message['id']} fixed ({old_path} -> {correct_path})")
                     
                 if (not config.dry_run):
                     conn.commit()
